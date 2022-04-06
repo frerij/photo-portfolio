26df0b03fc399cf878327bb4287a0606124e71a3
Jordan Frerichs <frerichs.jordan@gmail.com>
2022-04-06 13:08:20 -0700

    Commiting before editing models

--- projects/models.py
+++ projects/models.py
@@ -4,10 +4,10 @@ from django.conf import settings
 User_auth = settings.AUTH_USER_MODEL
 # Create your models here.
 
-
+# change model to collection
 class Project(models.Model):
-    name = models.CharField(max_length=200)
-    description = models.TextField()
+    name = models.CharField(max_length=200)  # change to title
+    description = models.TextField()  # description
     members = models.ManyToManyField(User_auth, related_name="projects")
 
     def __str__(self):
--- tasks/models.py
+++ tasks/models.py
@@ -4,16 +4,17 @@ from projects.models import Project, User_auth
 # Create your models here.
 
 
+#change model name to image
 class Task(models.Model):
-    name = models.CharField(max_length=200)
-    start_date = models.DateTimeField()
-    due_date = models.DateTimeField()
-    is_completed = models.BooleanField(default=False, null=False)
+    name = models.CharField(max_length=200) #change to title
+    start_date = models.DateTimeField() #change to date taken
+    due_date = models.DateTimeField() #add url/img link 
+    is_completed = models.BooleanField(default=False, null=False) #change to description
     project = models.ForeignKey(
-        Project, related_name="tasks", on_delete=models.CASCADE
+        Project, related_name="tasks", on_delete=models.CASCADE #change to collections
     )
     assignee = models.ForeignKey(
-        User_auth, null=True, related_name="tasks", on_delete=models.SET_NULL
+        User_auth, null=True, related_name="tasks", on_delete=models.SET_NULL #photographer
     )
 
     def __str__(self):
